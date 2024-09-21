64734b23bff760087288a68e63b37f1c2c21d85b
Abderrahmane Fethi <fethiabderrahmane1@gmail.com>
2024-09-21 17:58:36 +0100

    Enhancement

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/static/", groupie.HandleStatic)
 	http.HandleFunc("/", groupie.HandleHome)
 	http.HandleFunc("/informations/{id}", groupie.HandleInfos)
 	fmt.Println("Starting server on http://localhost:8080")
